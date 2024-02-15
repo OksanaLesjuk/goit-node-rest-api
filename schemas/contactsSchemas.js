b4173f649c311be7afcde5153ec0ef6889e91745
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2024-02-15 22:12:34 +0200

    test

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -3,13 +3,13 @@ const Joi = require("joi");
 const createContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    phone: Joi.string().required(),
+    phone: Joi.string().min(7).max(15).required(),
 })
 
 const updateContactSchema = Joi.object({
     name: Joi.string(),
     email: Joi.string().email(),
-    phone: Joi.string(),
+    phone: Joi.string().min(7).max(15),
 }).min(1) //що мінімум одна властивість має бути для оновленя
 
 
